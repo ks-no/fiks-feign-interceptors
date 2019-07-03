20fff0f708cf521e137ee2134589cc4d7564fdbd
Anders Flemmen <anders.flemmen@bouvet.no>
2019-07-03 12:50:40 +0200

    Access token interceptor setter alltid authorization header på nytt

--- src/main/java/no/ks/fiks/feign/AccessTokenInterceptor.java
+++ src/main/java/no/ks/fiks/feign/AccessTokenInterceptor.java
@@ -19,11 +19,7 @@ public class AccessTokenInterceptor implements RequestInterceptor {
 
     @Override
     public void apply(RequestTemplate template) {
-        if (template.headers().containsKey(AUTHORIZATION_HEADER)) {
-            log.debug("The Authorization token has been already set");
-        } else {
-            log.debug("Constructing Header {} for Token {}", AUTHORIZATION_HEADER, BEARER_TOKEN_TYPE);
-            template.header(AUTHORIZATION_HEADER, String.format("%s %s", BEARER_TOKEN_TYPE, accessTokenSupplier.get()));
-        }
+        log.debug("Constructing Header {} for Token {}", AUTHORIZATION_HEADER, BEARER_TOKEN_TYPE);
+        template.header(AUTHORIZATION_HEADER, String.format("%s %s", BEARER_TOKEN_TYPE, accessTokenSupplier.get()));
     }
 }
